fbfa080a3c16cc242f321d74f9aa8e97c5a96717
Vineet <green1@webenertia.com>
2022-08-06 23:05:06 -0400

    Script to get links from a page along with text

---
+++ get_links.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.hoover.org/'
+reqs = requests.get(url)
+soup = BeautifulSoup(reqs.text, 'html.parser')
+
+# opening a file in write mode
+f = open("links.txt", "w")
+for link in soup.find_all("a"):
+   data = link.get('href')
+   name = link.text
+   f.write(data+" : "+name)
+   f.write("\n")
+f.close()
